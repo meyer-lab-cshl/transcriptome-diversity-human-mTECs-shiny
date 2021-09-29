1496b35192a71f5acd8ee90962a787e5260efae0
HannahVMeyer <Hannah.v.meyer@gmail.com>
2021-09-29 17:23:15 -0400

    Combine all tracks in one plotTracks to align properly (move type to individual tracks); use new bedGraph files; adjust size; color background panel; include go left and right; set overall size

--- epitope_diversity/browser.R
+++ epitope_diversity/browser.R
@@ -7,11 +7,9 @@ library(BSgenome.Hsapiens.NCBI.GRCh38)
 library(shinydashboard)
 library(InteractiveComplexHeatmap)
 
-
 ## Import data objects ####
-library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+#library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 options(ucscChromosomeNames=FALSE)
-
 txdb <- makeTxDbFromEnsembl(organism="Homo sapiens",
                             release=NA,
                             circ_seqs=NULL,
@@ -19,14 +17,15 @@ txdb <- makeTxDbFromEnsembl(organism="Homo sapiens",
                             username="anonymous", password=NULL, port=0L,
                             tx_attrib=NULL)
 seqlevelsStyle(txdb) <- "UCSC"
-
 #txdb <- loadDb("data/txdb.db")
 
-bgFile_hi <- rtracklayer::import.bedGraph("data/tsr_tpm_hi_avg.bedGraph")
-bgFile_lo <- rtracklayer::import.bedGraph("data/tsr_tpm_lo_avg.bedGraph")
+bgFile_hi <- rtracklayer::import.bedGraph("data/tsr_tpm_hi_sum.bedGraph")
+bgFile_lo <- rtracklayer::import.bedGraph("data/tsr_tpm_lo_sum.bedGraph")
 
 ht_tpm <- readRDS("data/tpm_heatmap.rds")
 ht_lfc <- readRDS("data/fc_heatmap.rds")
+
+ht <- ht_tpm + ht_lfc
 
 ## Create annotations ####
 options(ucscChromosomeNames=FALSE)
@@ -51,69 +50,65 @@ alTrack_hi <- AlignmentsTrack(
     "data/mtec_hi_Aligned.sortedByCoord.out.bam",
     isPaired = TRUE,
     name = "mTEC hi RNAseq",
-    fill="#4c72b0ff")
+    background.title = "#4c72b0ff",
+    fill="#4c72b0ff",
+    type = c("coverage", "sashimi"))
 
 alTrack_lo <- AlignmentsTrack(
     "data/mtec_lo_Aligned.sortedByCoord.out.bam",
     isPaired = TRUE,
     name = "mTEC lo RNAseq",
-    fill="#dd8452ff")
+    background.title = "#dd8452ff",
+    fill="#dd8452ff",
+    type = c("coverage", "sashimi"))
 
 ## 5'Cap data
 alTrack_5cap_hi <- AlignmentsTrack(
     "data/5cap_mtec_hi_Aligned.sortedByCoord.out.bam",
     isPaired = TRUE,
     name = "mTEC hi 5Pseq",
-    fill="#4c72b0ff")
+    background.title = "#4c72b0ff",
+    fill="#4c72b0ff",
+    type = "coverage")
 
 alTrack_5cap_lo <- AlignmentsTrack(
     "data/5cap_mtec_lo_Aligned.sortedByCoord.out.bam",
     isPaired = TRUE,
     name = "mTEC lo 5Pseq",
-    fill="#dd8452ff")
+    background.title = "#dd8452ff",
+    fill="#dd8452ff",
+    type = "coverage")
 
-## Browser Tracks: Functions ####
+## processed TSR data
+data_track_hi <- DataTrack(range = bgFile_hi,
+                           genome = "hg38",
+                           name = "mTEC hi TSR",
+                           col ="#4c72b0ff",
+                           background.title = "#4c72b0ff",
+                           type = "h")
+
+data_track_lo <- DataTrack(range = bgFile_lo,
+                           genome = "hg38",
+                           name = "mTEC lo TSR",
+                           col ="#dd8452ff",
+                           background.title = "#dd8452ff",
+                           type = "h")
+
+
+## Plot Browser Tracks ####
 plot_genome <- function(location) {
-  # Load data, at a reasonable level of detail for width(location)
-  data_track_hi <- DataTrack(range = bgFile_hi, genome = "hg38", type = "h",
-                             name = "mTEC hi TSR",
-                             col ="#4c72b0ff",
-                             cex.sampleNames = 10)
-
-  data_track_lo <- DataTrack(range = bgFile_lo, genome = "hg38", type = "h",
-                             name = "mTEC lo TSR",
-                             col ="#dd8452ff",
-                             cex.sampleNames = 10)
-
   plotTracks(
     list(axis_track,
          seq_track,
          gene_track,
-         data_track_hi,
-         data_track_lo),
+         data_track_hi, data_track_lo,
+         alTrack_5cap_hi, alTrack_5cap_lo,
+         alTrack_hi, alTrack_lo
+         ),
     chromosome=as.character(seqnames(location)),
-    #sizes = c(0.5,1,2,3,3),
+    sizes = c(0.5,1,2,1,1,1,1,2,2),
     from=start(location), to=end(location),
     transcriptAnnotation="symbol")
-}
-
-plot_cap <- function(location) {
-  # Load data, at a reasonable level of detail for width(location)
-
-  plotTracks(
-    list(alTrack_5cap_hi, alTrack_5cap_lo),
-    chromosome=as.character(seqnames(location)),
-    from=start(location), to=end(location),
-    type = c("coverage"))
-}
-
-plot_sashimi <- function(location) {
-  # Load data, at a reasonable level of detail for width(location)
-  plotTracks(
-    list(alTrack_hi, alTrack_lo),
-    chromosome=as.character(seqnames(location)),
-    from=start(location), to=end(location),
-    type = c("coverage", "sashimi"))
 }
 
 browser_ui <- function(id) {
@@ -123,13 +118,17 @@ browser_ui <- function(id) {
       column(3,
              textInput(ns("location_str"),
                        "Genomic location",
-                       "chr11:1300000-1400000")),
+                       "chr1:1225000-1325000")),
       column(9,
+             actionButton(ns("go_left"), "<<"),
+             actionButton(ns("go_right"), ">>"),
              actionButton(ns("zoom_in"), "+"),
-             actionButton(ns("zoom_out"), "-"))),
-    plotOutput(ns("genome_plot")),
-    plotOutput(ns("cap_plot")),
-    plotOutput(ns("sashimi_plot")))
+             actionButton(ns("zoom_out"), "-"))
+      ),
+
+    plotOutput(ns("genome_plot"), height = "1200px"),
+
+    )
 }
 
 browser_server <- function(input, output, session) {
@@ -173,14 +172,6 @@ browser_server <- function(input, output, session) {
 
   output$genome_plot <- renderPlot({
     plot_genome( location() )
-  })
-
-  output$cap_plot <- renderPlot({
-    plot_cap( location() )
-  })
-
-  output$sashimi_plot <- renderPlot({
-    plot_sashimi( location() )
   })
 }
 
