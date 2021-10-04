130521c651c327f33cb60ed4b50c46a2d56c54d1
HannahVMeyer <Hannah.v.meyer@gmail.com>
2021-10-04 10:53:48 -0400

    use ideogram instead of sequence; adjust heights

--- epitope_diversity/browser.R
+++ epitope_diversity/browser.R
@@ -8,14 +8,16 @@ library(shinydashboard)
 library(InteractiveComplexHeatmap)
 
 ## Import data objects ####
-options(ucscChromosomeNames=FALSE)
-txdb <- makeTxDbFromEnsembl(organism="Homo sapiens",
-                            release=NA,
-                            circ_seqs=NULL,
-                            server="ensembldb.ensembl.org",
-                            username="anonymous", password=NULL, port=0L,
-                            tx_attrib=NULL)
-seqlevelsStyle(txdb) <- "UCSC"
+if (TRUE) {
+    options(ucscChromosomeNames=FALSE)
+    txdb <- makeTxDbFromEnsembl(organism="Homo sapiens",
+                                release=NA,
+                                circ_seqs=NULL,
+                                server="ensembldb.ensembl.org",
+                                username="anonymous", password=NULL, port=0L,
+                                tx_attrib=NULL)
+    seqlevelsStyle(txdb) <- "UCSC"
+}
 #txdb <- loadDb("data/txdb.db")
 
 bgFile_hi <- rtracklayer::import.bedGraph("data/tsr_tpm_hi_sum.bedGraph")
@@ -39,25 +41,24 @@ gene_annotation <- data.frame(
 ## Browser Tracks: Setting up ####
 ## Annotations
 axis_track <- GenomeAxisTrack()
-seq_track <- SequenceTrack(genome)
 gene_track <- GeneRegionTrack(
- txdb, genome = "hg38", name="Genes", showId=TRUE, shape="arrow",
- transcriptAnnotation = "symbol")
+    txdb, genome = "hg38", name="Genes", showId=TRUE, shape="arrow",
+    transcriptAnnotation = "symbol")
 
 ## RNAseq data
 alTrack_hi <- AlignmentsTrack(
+    #"data/5cap_mtec_hi_Aligned.sortedByCoord.out.bam",
     "data/mtec_hi_Aligned.sortedByCoord.out.bam",
     isPaired = TRUE,
     name = "mTEC hi RNAseq",
-    background.title = "#4c72b0ff",
     fill="#4c72b0ff",
     type = c("coverage", "sashimi"))
 
 alTrack_lo <- AlignmentsTrack(
+    #"data/5cap_mtec_hi_Aligned.sortedByCoord.out.bam",
     "data/mtec_lo_Aligned.sortedByCoord.out.bam",
     isPaired = TRUE,
     name = "mTEC lo RNAseq",
-    background.title = "#dd8452ff",
     fill="#dd8452ff",
     type = c("coverage", "sashimi"))
 
@@ -66,7 +67,6 @@ alTrack_5cap_hi <- AlignmentsTrack(
     "data/5cap_mtec_hi_Aligned.sortedByCoord.out.bam",
     isPaired = TRUE,
     name = "mTEC hi 5Pseq",
-    background.title = "#4c72b0ff",
     fill="#4c72b0ff",
     type = "coverage")
 
@@ -74,7 +74,6 @@ alTrack_5cap_lo <- AlignmentsTrack(
     "data/5cap_mtec_lo_Aligned.sortedByCoord.out.bam",
     isPaired = TRUE,
     name = "mTEC lo 5Pseq",
-    background.title = "#dd8452ff",
     fill="#dd8452ff",
     type = "coverage")
 
@@ -83,95 +82,106 @@ data_track_hi <- DataTrack(range = bgFile_hi,
                            genome = "hg38",
                            name = "mTEC hi TSR",
                            col ="#4c72b0ff",
-                           background.title = "#4c72b0ff",
                            type = "h")
 
 data_track_lo <- DataTrack(range = bgFile_lo,
                            genome = "hg38",
                            name = "mTEC lo TSR",
                            col ="#dd8452ff",
-                           background.title = "#dd8452ff",
                            type = "h")
 
 
 ## Plot Browser Tracks ####
 plot_genome <- function(location) {
-  plotTracks(
-    list(axis_track,
-         seq_track,
-         gene_track,
-         data_track_hi, data_track_lo,
-         alTrack_5cap_hi, alTrack_5cap_lo,
-         alTrack_hi, alTrack_lo
-         ),
-    chromosome=as.character(seqnames(location)),
-    sizes = c(0.5,1,2,1,1,1,1,2,2),
-    from=start(location), to=end(location),
-    transcriptAnnotation="symbol")
+    chr_name <- as.character(seqnames(location))
+    ideogram_track <- IdeogramTrack(genome = "hg38", chromosome = chr_name)
+
+    plotTracks(
+        list(ideogram_track,
+             axis_track,
+             gene_track,
+             data_track_hi, data_track_lo,
+             alTrack_5cap_hi, alTrack_5cap_lo,
+             alTrack_hi, alTrack_lo
+        ),
+        chromosome=chr_name,
+        sizes = c(0.1,
+                  0.5,
+                  2.5,
+                  0.8,
+                  0.8,
+                  1,
+                  1,
+                  2,
+                  2),
+        from=start(location), to=end(location),
+        background.title = "white",
+        col.title = "black",
+        col.axis = "black",
+        transcriptAnnotation="symbol")
 }
 
 browser_ui <- function(id) {
-  ns <- NS(id)
-  div(
-    fluidRow(
-      column(3,
-             textInput(ns("location_str"),
-                       "Genomic location",
-                       "chr1:1225000-1325000")),
-      column(9,
-             actionButton(ns("go_left"), "<<"),
-             actionButton(ns("go_right"), ">>"),
-             actionButton(ns("zoom_in"), "+"),
-             actionButton(ns("zoom_out"), "-"))
-      ),
-
-    plotOutput(ns("genome_plot"), height = "1200px"),
-
+    ns <- NS(id)
+    div(
+        div(textInput(
+            ns("location_str"),
+            "Genomic location",
+            "chr1:1250000-1300000"
+        ),
+        style = "margin-top:25px"),
+        div(
+            actionButton(ns("go_left"), "<<"),
+            actionButton(ns("go_right"), ">>"),
+            actionButton(ns("zoom_in"), "+"),
+            actionButton(ns("zoom_out"), "-")
+        ),
+        plotOutput(ns("genome_plot"), height = "1200px"),
     )
 }
 
 browser_server <- function(input, output, session) {
-  #To observe input (eg exactly what brushing provides):
-  #observe({ print(as.list(input)) })
+    #To observe input (eg exactly what brushing provides):
+    #observe({ print(as.list(input)) })
 
-  location <- reactive({
-    GRanges(input$location_str, seqinfo=seqinfo(genome))
-  })
+    location <- reactive({
+        GRanges(input$location_str, seqinfo=seqinfo(genome))
+    })
 
-  observeEvent(input$go_left, {
-    amount <- max(1, width(location()) %/% 4)
-    new_location <- shift(location(), -amount)
-    new_location <- trim(new_location)
-    updateTextInput(session, "location_str", value=as.character(new_location))
-  })
+    observeEvent(input$go_left, {
+        amount <- max(1, width(location()) %/% 4)
+        new_location <- shift(location(), -amount)
+        new_location <- trim(new_location)
+        updateTextInput(session, "location_str", value=as.character(new_location))
+    })
 
-  observeEvent(input$go_right, {
-    amount <- max(1, width(location()) %/% 4)
-    new_location <- shift(location(), amount)
-    new_location <- trim(new_location)
-    updateTextInput(session, "location_str", value=as.character(new_location))
-  })
+    observeEvent(input$go_right, {
+        amount <- max(1, width(location()) %/% 4)
+        new_location <- shift(location(), amount)
+        new_location <- trim(new_location)
+        updateTextInput(session, "location_str", value=as.character(new_location))
+    })
 
-  observeEvent(input$zoom_in, {
-    amount <- width(location()) %/% 4
-    new_location <- location()
-    start(new_location) <- start(new_location) + amount
-    end(new_location) <- end(new_location) - amount
-    updateTextInput(session, "location_str", value=as.character(new_location))
-  })
+    observeEvent(input$zoom_in, {
+        amount <- width(location()) %/% 4
+        new_location <- location()
+        start(new_location) <- start(new_location) + amount
+        end(new_location) <- end(new_location) - amount
+        updateTextInput(session, "location_str", value=as.character(new_location))
+    })
 
-  observeEvent(input$zoom_out, {
-    amount <- width(location()) %/% 2
-    new_location <- location()
-    start(new_location) <- start(new_location) - amount
-    end(new_location) <- end(new_location) + amount
-    new_location <- trim(new_location)
-    updateTextInput(session, "location_str", value=as.character(new_location))
-  })
+    observeEvent(input$zoom_out, {
+        amount <- width(location()) %/% 2
+        new_location <- location()
+        start(new_location) <- start(new_location) - amount
+        end(new_location) <- end(new_location) + amount
+        new_location <- trim(new_location)
+        updateTextInput(session, "location_str", value=as.character(new_location))
+    })
 
-  output$genome_plot <- renderPlot({
-    plot_genome( location() )
-  })
+    output$genome_plot <- renderPlot({
+        plot_genome( location() )
+    })
 }
 
 
