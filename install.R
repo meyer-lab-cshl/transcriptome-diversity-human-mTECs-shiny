bd9d0190a2d9e1c0a96b5c6f3a319e1411504723
sarah-chapin <sarah.rebecca.chapin@gmail.com>
2021-11-10 11:35:06 -0500

    added code to update version of BiocManager and GenomeInfoDb

--- install.R
+++ install.R
@@ -3,10 +3,14 @@ install.packages(c('devtools', 'magick', 'BiocManager', 'data.table',
                    'shinydashboard', 'RMariaDB'))
 
 ## Bioconductor
+
+###Install specific BiocManager version (3.14)
+BiocManager::install(version = "3.14")
+###Install packages
 BiocManager::install(c('GenomicRanges', 'Gviz', 'rtracklayer',
                        'TxDb.Hsapiens.UCSC.hg38.knownGene',
                        'BSgenome.Hsapiens.NCBI.GRCh38',
-                       'ComplexHeatmap')
+                       'ComplexHeatmap', 'GenomeInfoDb')
 )
 ## github ####
 # currently from github due to: https://www.biostars.org/p/9490625/
