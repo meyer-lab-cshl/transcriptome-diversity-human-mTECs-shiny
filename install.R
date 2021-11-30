12478a7769ff333bd1ffa581e60176af26a5afea
sarah-chapin <sarah.rebecca.chapin@gmail.com>
2021-11-30 16:40:58 -0500

    removed shinyWidgets package

--- install.R
+++ install.R
@@ -1,6 +1,6 @@
 ## CRAN ####
 install.packages(c('devtools', 'magick', 'BiocManager', 'data.table',
-                   'shinydashboard', 'RMariaDB', 'shiny', 'DT', 'shinyWidgets'), 
+                   'shinydashboard', 'RMariaDB', 'shiny', 'DT'), 
                  repos = "http://cran.us.r-project.org")
 
 ## Bioconductor
