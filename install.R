dfad3bb5001b8b7c79e82946d38581b13a79e42e
Chapin <chapin@bubblegum.cshl.edu>
2022-04-11 14:58:57 -0400

    added necessary packages

--- install.R
+++ install.R
@@ -1,12 +1,17 @@
 ## CRAN ####
 install.packages(c('devtools', 'magick', 'BiocManager', 'data.table',
-                   'shinydashboard', 'RMariaDB'))
+                   'shinydashboard', 'RMariaDB', 'shiny', 'DT', 'berryFunctions'),
+                 repos = "http://cran.us.r-project.org")
 
 ## Bioconductor
+
+###Install specific BiocManager version (3.14)
+BiocManager::install(version = "3.14")
+###Install packages
 BiocManager::install(c('GenomicRanges', 'Gviz', 'rtracklayer',
                        'TxDb.Hsapiens.UCSC.hg38.knownGene',
                        'BSgenome.Hsapiens.NCBI.GRCh38',
-                       'ComplexHeatmap')
+                       'ComplexHeatmap', 'GenomeInfoDb')
 )
 ## github ####
 # currently from github due to: https://www.biostars.org/p/9490625/
