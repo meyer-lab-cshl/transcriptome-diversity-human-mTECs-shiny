2b5daec3ba7dd10f023006306fc67616195d4c58
Chapin <chapin@bubblegum.cshl.edu>
2022-07-23 18:17:23 -0400

    changed bioconductor version

--- install.R
+++ install.R
@@ -6,13 +6,17 @@ install.packages(c('devtools', 'magick', 'BiocManager', 'data.table',
 ## Bioconductor
 
 ###Install specific BiocManager version (3.14)
-BiocManager::install(version = "3.14")
+#BiocManager::install()
+BiocManager::install(version = "3.15")
+###Install packages
 ###Install packages
 BiocManager::install(c('GenomicRanges', 'Gviz', 'rtracklayer',
                        'TxDb.Hsapiens.UCSC.hg38.knownGene',
                        'BSgenome.Hsapiens.NCBI.GRCh38',
-                       'ComplexHeatmap', 'GenomeInfoDb')
+		       'ComplexHeatmap', 'InteractiveComplexHeatmap',
+                       'GenomeInfoDb'), force = TRUE
 )
 ## github ####
 # currently from github due to: https://www.biostars.org/p/9490625/
+#devtools::install_github("jokergoo/ComplexHeatmap")
 devtools::install_github("jokergoo/InteractiveComplexHeatmap")
